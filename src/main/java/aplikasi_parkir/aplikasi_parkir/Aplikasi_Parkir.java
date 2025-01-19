0e85d426d2a72a8ccfd939302cd77b1dc3067f77
annisaarifani <annisaarifani1512@gmail.com>
2025-01-19 15:05:17 +0700

    Add files via upload

---
+++ src/main/java/aplikasi_parkir/aplikasi_parkir/Aplikasi_Parkir.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aplikasi_parkir.aplikasi_parkir;
+
+/**
+ *
+ * @author Aldian Yudhanto
+ */
+public class Aplikasi_Parkir {
+
+    public static void main(String[] args) {
+        Frame_aplikasi_parkir Laund = new Frame_aplikasi_parkir();
+        Laund.setVisible(true);
+    }
+}
